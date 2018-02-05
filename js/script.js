c1c529cefb6c59e0b0eb0b596e050a26f1c9a0e9
Wendy Jiang <wjiang.student@hsfi.us>
2018-02-05 23:22:31 +0000

    fix myplaylist

--- js/script.js
+++ js/script.js
@@ -19,28 +19,29 @@
 var mySong = {
 	"title":"24K Magic",
 	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+	"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 }
+
 
 var myPlayList = [
 	{
 		"title":"24K Magic",
 		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
 		"title":"Sir Duke",
 		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"imageurl":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
 		"title":"Sorry",
 		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"mp3url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"imageurl":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -51,10 +52,14 @@ var myPlayList = [
 $( document ).ready(function() {
   
 
-
+$("body").append("<p>Title: " + mySong.title + "</p>");
+$("body").append("<p>Artist: " + mySong.artist + "</p>");
+$("body").append("<p>MP3: <a href=" + mySong.mp3url + ">Link</a></p>");
+$("body").append("<img src=" +mySong.imageurl + ">");
 
 });
 
+$("body").append("<p>Title: " +myPlayList(1).title + "</p>");
 function displayList(){
 
 
