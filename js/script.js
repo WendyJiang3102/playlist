9592f23160e40669b50682d09132e0f6402392b4
Wendy Jiang <wjiang.student@hsfi.us>
2018-03-06 12:18:23 +0000

    change playlist

--- js/script.js
+++ js/script.js
@@ -20,105 +20,100 @@
 //	"title":"24K Magic",
 //	"artist":"Bruno Mars",
 //	"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-//	"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",}
+//	"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+//}
 
-
-var myPlayList = [
-	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-	},
-	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"imageurl":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
-	},
-	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"imageurl":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
-	}
-
-];
+    var myPlayList = [
+    	{
+    		"title":"24K Magic",
+    		"artist":"Bruno Mars",
+    		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+    		"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+    	},
+    	{
+    		"title":"Sir Duke",
+    		"artist":"Stevie Wonder",
+    		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+    		"imageurl":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+    	},
+    	{
+    		"title":"Sorry",
+    		"artist":"Justin Bieber",
+    		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+    		"imageurl":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+    	}
+    
+    ];
 
 
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  
-
-//$("body").append("<p>Title: " + mySong.title + "</p>");
-//$("body").append("<p>Artist: " + mySong.artist + "</p>");
-//$("body").append("<p>MP3: <a href=" + mySong.mp3url + ">Link</a></p>");
-//$("body").append("<img src=" +mySong.imageurl + ">");
-
-myPlayList.forEach(function(song){
-
-	var title = song.title;
-	var artist = song.artist;
-	var url = song.mp3url;
-	var image = song.imageurl;
-
-	$("body").append(`
-		
-		<div class = "songs">
-			
-			<h3 class = "word">${title}</h3>
-			<a class = "word"> ${artist}</a>
-			<img src = ${image}>
-			<a href = ${url}>Play Song</a>
-			
-		</div>
-
-	`);
-
-});
-
-
-
-function displayList(){
-	for (var i = 0; i < myPlayList.length; i++){
-		$(".songs").append('<p>' + myPlayList[i].title +'<p>');
-        $(".songs").append('<p>' + myPlayList[i].artist +'<p>');
-	}
-
-  
-}
-
-function clearList(){
-  
-	$('.songs').empty();
-  
-}
-
-function addSong(){
-	var title=$("#title").val();
-	var artist=$("#artist").val();
-	var linkInput=$("#play-link").val();
-    var imageInput=$("#album-image").val();
-    
-	var newSong={
-		title:title,
-        artist:artist,
-        link:linkInput,
-        image:imageInput
+//	$('.songs').append("<p>Title: " + mySong.title + "</p>");
+//	$('.songs').append("<p>Artist: " + mySong.artist + "</p>");
+//	$('.songs').append("<p>MP3: <a href=" +mySong.mp3url+ "> 24K Magic </a></p>");
+//	$('.songs').append("<img src=" + mySong.imageurl + ">");
+//	$('.songs').append("<p>Title: " + myPlayList.title + "</p>");
+//	$('.songs').append("<p>Artist: " + myPlayList.artist + "</p>");
+//	$('.songs').append("<p>MP3: <a href=" +myPlayList.mp3url+ "> 24K Magic </a></p>");
+//	$('.songs').append("<img src=" + myPlayList.imageurl + ">");
+//	myPlayList.forEach(function(songs) {
+//    var title = songs.title;
+//    var artist = songs.artist;
+//    var url = songs.mp3url;
+//    var image = songs.imageurl;
+ 
+//       $("body").append(`
+//    		<div class="songs">
+//            	<h3>Title: ${title}</h3>
+//            	<a>Artist: ${artist}</a>
+//            	<img src="${image}">
+//            	<a href= ${url}> Play Song</a> 
+//           </div>
+//        `);
         
-	};
+//    });
+    
+    $('#addSong').click(function(){
+        clearList();
+        addSong();
+        displayList();
+     
+    });
+    
   
-  myPlayList.push(newSong);
-}
-	
-	$("#new").click(function(){
-   		clearList();
-	  	addSong();
-   		displayList();
+    function displayList(){
+        
+    	for (var i = 0; i < myPlayList.length; i++){
+            $('.songs').append('<p>' + myPlayList[i].title+'<p>');
+            $('.songs').append('<p>' + myPlayList[i].artist +'<p>');
+            $('.songs').append("<p>MP3: <a href=" +myPlayList[i].mp3url+ ">Link</a></p>");
+            $('.songs').append("<img src=" + myPlayList[i].imageurl + ">");
+        }
+    }
+    
+    function clearList(){
+       $('.songs').empty();
+      
+    }
+    
+    function addSong(){
+    	var titleInput = $("#title").val();
+        var artistInput = $("#artist").val();
+        var urlInput = $("#mp3url").val();
+        var imageInput = $("#imageurl").val();
+        
+        var song = {
+            title: titleInput,
+            artist: artistInput,
+            url: urlInput,
+            image: imageInput
+            };
+        myPlayList.push(song);
+        
+        
+    }
+    
+    displayList();
 
-	
-	});
-	console.log(myPlayList);
-	
 });
